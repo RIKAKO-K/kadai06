83a5e1747c76155c3afcc50ac5fa55fd2f187311
RIKAKO <rikako.k.8637@gmail.com>
2023-12-06 01:45:38 +0900

    第6回課題

---
+++ src/test/java/com/helloworld/kadai06/kadai06ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.helloworld.kadai06;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class kadai06ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
