f127bd87a10dc7e8fea4f96cb4b81a10e10b3dfd
RIKAKO <rikako.k.8637@gmail.com>
2023-12-06 02:51:41 +0900

    表示内容を変更

---
+++ src/main/java/com/helloworld/kadai06/MYFIRSTSTORY.java
@@ -0,0 +1,19 @@
+package com.helloworld.kadai06;
+
+public class MYFIRSTSTORY {
+    private  final  String name;
+    private  final int age;
+
+    public MYFIRSTSTORY(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+}
